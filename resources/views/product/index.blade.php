997ba02fa2ad151e015df7a33fb010b9291b08f8
Majdsayedtaha <54773986+Majdsayedtaha@users.noreply.github.com>
2021-06-03 23:17:36 +0300

    style the search bar

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,7 +4,7 @@
 @endsection
 
 @section('content')
-<div class="col-md-6">
+<div class="col-md-6 serch-container">
     <div class="header-search">
         <form>
             <select class="input-select">
@@ -47,18 +47,18 @@
                             <div id="tab1" class="tab-pane active">
                                 <div class="products-slick" data-nav="#slick-nav-1">
                                     <!-- product -->
+                                    @for ($i = 0; $i < 10; $i++)
                                     <div class="product">
                                         <div class="product-img">
                                             <img src="./img/product01.png" alt="">
                                             <div class="product-label">
-                                                <span class="sale">-30%</span>
                                                 <span class="new">NEW</span>
                                             </div>
                                         </div>
                                         <div class="product-body">
-                                            <p class="product-category">Category</p>
+                                            <p class="product-category">Category Name</p>
                                             <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                            <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+                                            <h4 class="product-price">$980.00</h4>
                                             <div class="product-rating">
                                                 <i class="fa fa-star"></i>
                                                 <i class="fa fa-star"></i>
@@ -69,8 +69,6 @@
                                             <div class="product-btns">
                                                 <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span
                                                     class="tooltipp">add to wishlist</span></button>
-                                                <button class="add-to-compare"><i class="fa fa-exchange"></i><span
-                                                        class="tooltipp">add to compare</span></button>
                                                         <button class="quick-view"><i class="fa fa-eye"></i><span
                                                         class="tooltipp">quick view</span></button>
                                             </div>
@@ -80,8 +78,9 @@
                                                 cart</button>
                                             </div>
                                     </div>
+                                    @endfor
                                     <!-- /product -->
-                                    <!-- product -->
+                                    {{-- <!-- product -->
                                     <div class="product">
                                         <div class="product-img">
                                             <img src="./img/product02.png" alt="">
@@ -205,7 +204,7 @@
                                                 cart</button>
                                         </div>
                                     </div>
-                                    <!-- /product -->
+                                    <!-- /product --> --}}
                                 </div>
                                 <div id="slick-nav-1" class="products-slick-nav"></div>
                             </div>
