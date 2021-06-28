fccb0a20524e0fc33ea7729ccc1f56b5a6ccd323
abd-wazzan <abdulrahmanwazzan.pro@gmail.com>
2021-06-29 00:35:25 +0300

    restore header.

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -20,63 +20,53 @@
                         <div class="header-ctn">
                             @auth
                             <!-- Wishlist -->
-                            <div class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                            <div>
+                                <a href="#">
                                     <i class="fa fa-heart-o"></i>
                                     <span>Your Wishlist</span>
                                     <div class="qty">{{auth()->user()->favorites->count()}}</div>
                                 </a>
-                                <div class="cart-dropdown">
-                                    <div class="cart-list">
-                                        {{-- @foreach (auth()->user()->favorites as $wishlist)
-                                        <div class="product-widget">
-                                            <div class="product-img">
-                                                <img src="{{$wishlist->product->img ?? "./img/product02.png"}}" alt="">
-                                            </div>
-                                            <div class="product-body">
-                                                <h3 class="product-name"><a href="#">{{$wishlist->product->name}}</a></h3>                                            </div>
-                                            <button onclick="toggleCart({{$wishlist->product_id}})" class="delete"><i class="fa fa-close"></i></button>
-                                        </div>
-                                        @endforeach --}}
-                                    </div>
-                                </div>
                             <!-- /Wishlist -->
                             </div>
-                            <!-- Cart -->
-                            <div class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    <span>Your Cart</span>
-                                    <div class="qty">{{auth()->user()->carts->count()}}</div>
-                                </a>
-                                <div class="cart-dropdown">
-                                    <div class="cart-list">
-                                        @foreach (auth()->user()->carts as $cart)
-                                        <div class="product-widget">
-                                            <div class="product-img">
-                                                <img src="{{$cart->product->img ?? "./img/product02.png"}}" alt="">
+                                <!-- Cart -->
+                                <div class="dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                                        <i class="fa fa-shopping-cart"></i>
+                                        <span>Your Cart</span>
+                                        <div class="qty">{{auth()->user()->carts->count()}}</div>
+                                    </a>
+                                    <div class="cart-dropdown">
+                                        <div class="cart-list">
+                                            @foreach (auth()->user()->carts as $cart)
+                                            <div class="product-widget">
+                                                <div class="product-img">
+                                                    <img src="{{$cart->product->img ?? "./img/product02.png"}}" alt="">
+                                                </div>
+                                                <div class="product-body">
+                                                    <h3 class="product-name"><a href="#">{{$cart->product->name}}</a></h3>
+                                                    <h4 class="product-price"><span class="qty">{{$cart->qty}}x</span>${{$cart->product->price}}</h4>
+                                                </div>
+                                                <button onclick="toggleCart({{$cart->product_id}})" class="delete"><i class="fa fa-close"></i></button>
                                             </div>
-                                            <div class="product-body">
-                                                <h3 class="product-name"><a href="#">{{$cart->product->name}}</a></h3>
-                                                <h4 class="product-price"><span class="qty">{{$cart->qty}}x</span>${{$cart->product->price}}</h4>
-                                            </div>
-                                            <button onclick="toggleCart({{$cart->product_id}})" class="delete"><i class="fa fa-close"></i></button>
+                                            @endforeach
                                         </div>
-                                        @endforeach
-                                    </div>
-                                    <div class="cart-summary">
-                                        <small>{{auth()->user()->carts->count()}} Item(s) selected</small>
-                                        <h5>SUBTOTAL: ${{auth()->user()->cartProducts()->sum('price')}}</h5>
+                                        <div class="cart-summary">
+                                            <small>{{auth()->user()->carts->count()}} Item(s) selected</small>
+                                            <h5>SUBTOTAL: ${{auth()->user()->cartProducts()->sum('price')}}</h5>
+                                        </div>
+                                        <div class="cart-btns">
+                                            <a href="#">View Cart</a>
+                                            <a href="#">Checkout <i class="fa fa-arrow-circle-right"></i></a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <!-- /Cart -->
+                                <!-- /Cart -->
 
                             <!-- show Your Products -->
                             <div class="add-product">
                                 <a href="{{route('store.show', auth()->user()->id)}}">
-                                    <i class="fas fa-store-alt"></i>
-                                    <span>My Products</span>
+                                    <i class="fas fa-shopping-basket" ></i>
+                                    <span> View Products</span>
                                 </a>
                             </div>
                             <!-- /show Your Products-->
@@ -84,7 +74,7 @@
                             <!-- Add a product -->
                             <div class="add-product">
                                 <a href="{{route('chose-cat')}}">
-                                    <i class="fas fa-cart-plus"></i>
+                                    <i class="fa fa-plus" ></i>
                                     <span>Add product</span>
                                 </a>
                             </div>
